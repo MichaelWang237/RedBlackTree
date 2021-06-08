13a96e0a02ae2f0f066273159bc7c26594388c26
MichaelWang237 <79058098+MichaelWang237@users.noreply.github.com>
2021-06-08 02:55:58 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,9 +17,7 @@
 
 using namespace std;
 
-//Node structure
-//Each node contains an integer value (data) and color (isRed) and is also linked to its children (left and right) and parent nodes.
-
+//Each node contains a data value (int), a boolean value to keep track of color and is linked to its left and right children as well as its parent.
 struct node{
     int data;
     bool isRed = true;
@@ -28,129 +26,22 @@ struct node{
     node* parent = NULL;
 };
 
-//Function Prototypes
+//function prototypes below 
 void print(node* root, int spacing);
 void add(node* root, node* newNode);
-void insert(node*&root, node* n);
 void rotateL(node*&root, node* n);
 void rotateR(node*&root, node* n);
+void insert(node*&root, node* n);
+//PART2
 node* search(node* root, int a);
 bool deleteNode(node*& root, node*n);
-void replace(node*& n, node* child);
+node* Successor(node* n);
+node* Sibling(node* n);
 void fix(node*&root, node* n);
 
-int main(){
-    node* root = new node;
-    bool isNotEmpty = false;
-    while(1){
-        cout << "Type 'add' to add a series of numbers, 'print' to display the tree, 'read' to create a tree from a file, 'search' to verify whether a given value is present, 'delete' to remove nodes of a certain value, and 'quit' to quit." << endl;
-        char* input = new char[10];
-        int* array = new int[100];
-        int index = 0;
-        bool addCommand = false;
-        cin.clear();
-        cin.getline(input, 10);
-        if(strcmp(input, "add") == 0){
-            char* temp = new char[500];
-            cout << "Type integers below. Make sure to separate with spaces." << endl;
-            cin.getline(temp, 500);
-            char* token = strtok(temp, " ");
-            while(token != NULL){
-                array[index] = atoi(token);
-                token = strtok(NULL, " ");
-                index++;
-            }
-            addCommand = true;
-        }
-        else if(strcmp(input, "read") == 0){
-            cout << "Enter a file name, including the format, i.e. [fileName.txt]. Make sure the numbers in the file are separated by spaces." << endl;
-            string fileName;
-            cin.clear();
-            cin >> fileName;
-            fstream file;
-            int temp = 0;
-            file.open(fileName, ios::in);
-            while(file >> temp){
-                array[index] = temp;
-                index++;
-            }
-            addCommand = true;
-        }
-        else if(strcmp(input, "print") == 0){
-            print(root, 0);
-        }
-        else if(strcmp(input, "search") == 0){
-            int a = 0;
-            cout << "What is the value of the integer you would like to search for?" << endl;
-            cin >> a;
-            cin.get();
-            if(search(root, a) != NULL){
-                cout << "This number exists in the tree." << endl;
-            }
-            else{
-                cout << "This number isn't in the tree." << endl;
-            }
-        }
-        else if(strcmp(input, "delete") == 0){
-                bool sameTree = true;
-                char* temp = new char[500];
-                cout << "Separate integers with spaces." << endl;
-                cin.getline(temp, 500);
-                char* token = strtok(temp, " ");
-                while(token != NULL){
-                    array[index] = atoi(token);
-                    token = strtok(NULL, " ");
-                    index++;
-                }
-                bool treeNotEmpty = true;
-                int i = 0;
-                while(i < index){
-                    node* x = search(root, array[i]);
-                    if(x != NULL){
-                        cout << "The nodes containing the following value(s) will now be deleted:" << array[i]<< endl;
-                        treeNotEmpty = deleteNode(root, x);
-                        if(treeNotEmpty == false){
-                            cout << "Deletion completed." << endl;
-                            sameTree = false;
-                            break;
-                        }
-                    }
-                    else{
-                        cout << "This node couldn't be found in the tree."<< endl;
-                    }
-                    i++;
-                }
-                delete [] array;
-        }
-        else if(strcmp(input, "quit") == 0){
-            cout << "quitting..." << endl;
-            exit(0);
-        }
-        else{
-            cout << "Enter a valid command" << endl;
-        }
-        if(addCommand == true){
-            int i = 0;
-            //If the tree is empty, set root to black.
-            if(isNotEmpty == false){ 
-                root->data = array[0];
-                root->isRed = false;
-                i++;
-                isNotEmpty = true;
-            }
-            while(i < index){
-                node* n = new node;
-                n->data = array[i];
-                //insert(root, n);
-                insert(root, n);
-                i++;
-            }
-            delete [] array;
-        }
-    }
-    return 0;
-}
-//Displays the red black tree through recursion
+//part 1 of the project is directly below, followed by MAIN. Immediately below MAIN are the part 2 functions -- search and delete -- as well as some associated functions.
+
+//Prints tree through recursion
 void print(node* root, int spacing){
     if(root == NULL){
         return;
@@ -162,18 +53,20 @@ void print(node* root, int spacing){
         cout << " ";
     }
     if(root->isRed){
-        cout << "[R]";
+        cout << "[r]";
     }
     else{
-        cout << "[B]";
+        cout << "[b]";
     }
     cout << root->data << '\n';
     print(root->left, spacing);
 }
+
 //This function is used by the "insert" function and simply adds the node to the correct position without doing anything to maintain the properties of a red-black tree.
 void add(node* root, node* n){
     node* temp = root;
     while(1){
+
         if(n->data >= temp->data){
             if(temp->right == NULL){
                 temp->right = n;
@@ -196,15 +89,63 @@ void add(node* root, node* n){
         }
     }
 }
+
+void rotateL(node*& root, node* n){
+    //function for left rotation around node n
+    cout << "LR" << endl;
+    node* right = n->right;
+    node* temp = right->left;
+    node* parent = n->parent;
+    if(n == root){
+        cout << "NOTE: root was affected by this rotation" << endl;
+        root = right;    
+    }
+    else if(n == n->parent->left){
+        n->parent->left = right;
+    }
+    else if(n == n->parent->right){
+        n->parent->right = right;
+    }
+    n->parent = right;
+    n->right = temp;
+    if(temp != NULL) temp->parent = n;
+    right->left = n;
+    right->parent = parent;
+}
+
+void rotateR(node*&root, node* n){
+    //function for right rotation around node n
+    cout << "RR"<< endl;
+    node* left = n->left;
+    node* temp = left->right;
+    node* parent = n->parent;
+    if(n->parent == NULL){
+        cout << "NOTE: root was affected by this rotation" << endl;
+        root = left;
+        root->parent = NULL;
+    }
+    else if(n == n->parent->left){
+        n->parent->left = left;
+    }
+    else if(n == n->parent->right){
+        n->parent->right = left;
+    }
+    n->parent = left;
+    n->left = temp;
+    if(temp != NULL) temp->parent = n;
+    left->parent = parent;
+    left->right = n;
+    
+}
 //Using the "add" function, this function puts the node on the tree and does the necessary changes to ensure that the tree follows the rules of a red-black tree
 //This is done by checking the new node's relatives against every case that violates the properties of a red-black tree.
 void insert(node*& root, node* n){
-    add(root, n);
     //If the node's parent is black and the node itself is red, then no changes need to be made. However, if the parent is red, then further analysis needs to be done.
+    add(root, n);
     if(n->parent->isRed){
         node* uncle = NULL;
         bool isUncleR = false;
-        while((n != root) && (n->parent->isRed)){
+        while((n != root) && (n->parent->isRed)){ 
             if(n->parent != root && n != root){
                 if(n->parent == n->parent->parent->left){
                     uncle = n->parent->parent->right;
@@ -240,7 +181,6 @@ void insert(node*& root, node* n){
                 }
                 n->parent->parent->isRed = true;
                 n->parent->isRed = false;
-
                 if(isUncleR){
                     rotateR(root, n->parent->parent);
                 }
@@ -252,54 +192,140 @@ void insert(node*& root, node* n){
     }
     root->isRed = false;
 }
-//This function performs a left tree rotation
-void rotateL(node*& root, node* n){
-    cout << "left rotation started" << " on node: " << n->data << endl;
-    node* right = n->right;
-    node* temp = right->left;
-    node* parent = n->parent;
-    if(n == root){
-        cout << "root changed" << endl;
-        root = right;    
+
+int main(){
+    bool running = true;
+    while(running){
+        node* root;
+        bool initRoot = false;
+        bool sameTree = true;
+        while(sameTree == true){
+            cout << "Type 'add' to add a series of numbers, 'print' to display the tree, 'read' to create a tree from a file, 'search' to verify whether a given value is present, 'delete' to remove nodes of a certain value, and 'quit' to quit." << endl;
+            int index = 0;
+            int* array = new int[100];
+            char* input = new char[10];
+            bool addCommand = false;
+            cin.clear();
+            cin.getline(input, 10);
+            
+            //Implementation of ADD function in main
+            if(strcmp(input, "add") == 0){
+                char* temp = new char[500];
+                cout << "Type integers below. Make sure to separate with spaces."  << endl;
+                cin.getline(temp, 500);
+                char* token = strtok(temp, " ");
+                while(token != NULL){
+                    array[index] = atoi(token);
+                    token = strtok(NULL, " ");
+                    index++;
+                }
+                addCommand = true;
+            }
+
+            //Implementation of READ function in main
+            else if(strcmp(input, "read") == 0){
+                cout << "Enter a file name, including the format, e.g. [fileName.txt]. Make sure the numbers in the file are separated by spaces." << endl;
+                string fileName;
+                cin.clear();
+                cin >> fileName;
+                fstream file;
+                int temp = 0;
+                file.open(fileName, ios::in);
+                while(file >> temp){
+                    array[index] = temp;
+                    index++;
+                }
+                addCommand = true;
+            }
+
+            //Implementation of PRINT function in main
+            else if(strcmp(input, "print") == 0){
+                print(root, 0);
+            }
+
+            //Implementation of SEARCH function in main
+            else if(strcmp(input, "search") == 0){
+                int temp = 0;
+                cout << "What is the value of the integer you would like to search for?" << endl;
+                cin >> temp;
+                node* n = search(root, temp);
+                if(n == NULL){
+                    cout << "Node could not be found" << endl;
+                }
+                else{
+                    cout << "This node exists in the tree. Somewhere." << endl;
+                }
+            }
+
+            //Implementation of DELETE function in main
+            else if(strcmp(input, "delete") == 0){
+                char* temp = new char[500];
+                cout << "Separate each integer you want to delete with a space." << endl;
+                cin.getline(temp, 500);
+                char* token = strtok(temp, " ");
+                while(token != NULL){
+                    array[index] = atoi(token);
+                    token = strtok(NULL, " ");
+                    index++;
+                }
+                bool treeNotEmpty = true;
+                int i = 0;
+                while(i < index){
+                    node* x = search(root, array[i]);
+                    if(x != NULL){
+                        treeNotEmpty = deleteNode(root, x); // <----- Delete function called here
+                        if(treeNotEmpty == false){
+                            sameTree = false;
+                            break;
+                        }
+                    }
+                    else{
+                        cout << "couldn't find " << array[i] << endl;
+                    }
+                    i++;
+                }
+                delete [] array;
+            }
+
+            //Implementation of quit option
+            else if(strcmp(input, "quit") == 0){
+                cout << "quitting..." << endl;
+                running  = false;
+                sameTree = false;
+                exit(0);
+            }
+            //If the input doesn't match any commands, the user is asked to input a valid command.
+            else{
+                cout << "Enter a valid command" << endl;
+            }
+            if(addCommand == true){
+                int i = 0;
+                //If the tree is empty, set root to black.
+                if(initRoot == false){ 
+                    root = new node;
+                    root->data = array[0];
+                    root->isRed = false;
+                    i++;
+                    initRoot = true;
+                }
+                while(i < index){
+                    node* n = new node;
+                    n->data = array[i];
+                    insert(root, n);
+                    i++;
+                }
+                delete [] array;
+            }
+        }
     }
-    else if(n == n->parent->left){
-        n->parent->left = right;
-    }
-    else if(n == n->parent->right){
-        n->parent->right = right;
-    }
-    n->parent = right;
-    n->right = temp;
-    right->left = n;
-    right->parent = parent;
-    cout << "left rotation completed" << endl;
-}
-//This function performs a right tree rotation
-void rotateR(node*&root, node* n){
-    cout << "right rotation started" << " on node: " << n->data << endl;
-    node* left = n->left;
-    node* temp = left->right;
-    node* parent = n->parent;
-    if(n->parent == NULL){
-        cout << "root changed" << endl;
-        root = left;
-        root->parent = NULL;
-    }
-    else if(n == n->parent->left){
-        n->parent->left = left;
-    }
-    else if(n == n->parent->right){
-        n->parent->right = left;
-    }
-    n->parent = left;
-    n->left = temp;
-    left->parent = parent;
-    left->right = n;
-    cout << "right rotation completed" << endl;
+    return 0;
 }
 
-//This function looks for nodes with a given value a. If they exist, the function returns them. If not, it returns NULL.
+
+
 node* search(node* root, int a){
+    //function can be called to check the existence of a node in the tree based on its integer value
+    //if the node exists, it is returned, if not, the function returns NULL
     node* n = new node;
     n = root;
     while(1){
@@ -320,8 +346,8 @@ node* search(node* root, int a){
         }
     }
 }
-//This function finds the in-order successor of a node n
-node* Successor(node* n){
+
+node* Successor(node* n){ //function to find in-order successor to a node
     node* temp = n;
     if(temp->left != NULL){
         temp = temp->left;
@@ -338,8 +364,7 @@ node* Successor(node* n){
     return temp;
 }
 
-//This function finds and returns the sibling of a given node n.
-node* Sibling(node* n){
+node* Sibling(node* n){ //finds the sibling of the given node
     if(n == n->parent->left){
         return n->parent->right;
     }
@@ -348,9 +373,9 @@ node* Sibling(node* n){
     }
 }
 
-//This function deletes a node n while maintaining the properties of a red-black tree (which is why it's so long).
-bool deleteNode(node*& root, node* n){
-    if(n == root && root->right == NULL && root->left == NULL){
+//This function is ultimately responsible for deletion while maintaining the properties of a red black tree.
+bool deleteNode(node*& root, node* n){ 
+    if(n == root && root->right == NULL && root->left == NULL){ 
         root = NULL;
         delete root;
         return false;
@@ -362,16 +387,15 @@ bool deleteNode(node*& root, node* n){
         n = s;
     }
 
-    //There are 3 cases if n is black. The code below handles these cases.
-    if(!n->isRed){
+    //IF n is a black node... The code below handles each case.
+    if(!n->isRed){ 
         if(n->left != NULL){
-            cout << "n is black, child on left" << endl;
             n->data = n->left->data;
             n = n->left;
             deleteNode(root, n);
         }
         else if(n->right != NULL){
-            cout << "n is black, child on right" << endl;
+
             n->data = n->right->data;
             n = n->right;
             deleteNode(root, n);
@@ -380,6 +404,7 @@ bool deleteNode(node*& root, node* n){
             node* sibling = Sibling(n);
             if(n == n->parent->left){
                 if(sibling->isRed){
+
                     sibling->isRed = false;
                     rotateL(root, n->parent);
                     sibling = Sibling(n);
@@ -396,7 +421,6 @@ bool deleteNode(node*& root, node* n){
                 if(!sibling->isRed){
 
                     if(sibling->right != NULL && sibling->right->isRed){
-
                         Sibling(n)->right->isRed = false;
                         rotateL(root, n->parent);
                         n->parent->left = NULL;
@@ -443,13 +467,13 @@ bool deleteNode(node*& root, node* n){
                     return true;
                 }
                 if(!sibling->isRed){
+
                     if(sibling->left != NULL && sibling->left->isRed){
                         Sibling(n)->left->isRed = false;
                         rotateR(root, n->parent);
                         n->parent->right = NULL;
                         delete n;
                     }
-
                     else if(sibling->left != NULL && sibling->right->isRed){
                         Sibling(n)->left->isRed = false;
                         Sibling(n)->right->isRed = true;
@@ -477,17 +501,20 @@ bool deleteNode(node*& root, node* n){
     }
     //In case n is red, this section takes care of all 3 possibilities.
     else if(n->isRed){ 
-        if(n->left != NULL){
+        if(n->left != NULL){ 
+
             n->data = n->left->data;
             n->isRed = false;
             n->left = NULL;
         }
         else if(n->right != NULL){
+
             n->data = n->right->data;
             n->isRed = false;
             n->right = NULL;
         }
         else{
+
             if(n == n->parent->left){
                 n->parent->left = NULL;
             }
@@ -501,7 +528,7 @@ bool deleteNode(node*& root, node* n){
 }
 
 //This function moves up the tree to take care of problems caused by other operations.
-void fix(node*&root, node* n){
+void fix(node*&root, node* n){ 
     while(n != root && !n->isRed){
         if(n == n->parent->left){
             node* sibling = n->parent->right;
